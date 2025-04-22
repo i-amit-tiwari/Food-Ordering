ac098dfaae90d7efb94ec6668c445082fe0b2f9b
Amit Tiwari <138901423+i-amit-tiwari@users.noreply.github.com>
2025-04-22 09:52:11 +0000

    food-app

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV == "production" &&
+    process.env.REPL_ID == undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
